de5ea91d6ffe254b2fb0da433727e0f833425713
krunal <krunalpabari11@gmail.com>
2024-10-26 22:35:34 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/Marknox-web-1/', // Correctly formatted with leading slash and quotes
 });
